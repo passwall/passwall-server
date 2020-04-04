49fe58600fb55799b1f7e3c6801a6b8931bed084
Erhan Yakut <erhan.yakut@icsdefense.net>
2020-04-05 02:09:38 +0300

    delete forgotton read function

--- main.go
+++ main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"gpass/model"
 	"gpass/pkg/config"
 	"gpass/pkg/database"
 	"gpass/pkg/router"
-	"log"
-	"os"
-	"strings"
 )
 
 func init() {
@@ -21,30 +16,4 @@ func main() {
 
 	r := router.Setup()
 	r.Run("127.0.0.1:" + config.Server.Port)
-}
-
-func read() {
-	db := database.GetDB()
-	file, err := os.Open("./yedek.csv")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		dizi := strings.Split(scanner.Text(), ",")
-		login := model.Login{
-			URL:      dizi[4], // 1password URL field
-			Username: dizi[5], // 1password username field
-			Password: dizi[1], // 1password password field
-		}
-
-		db.Create(&login)
-		// fmt.Println(scanner.Text())
-	}
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
 }
