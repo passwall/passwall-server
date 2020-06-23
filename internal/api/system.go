b46c975a849cd6265e898987913adfa98bf082c8
Erhan Yakut <yakuter@gmail.com>
2020-06-24 00:07:48 +0300

    update version check

--- internal/api/system.go
+++ internal/api/system.go
@@ -41,7 +41,7 @@ func CheckUpdate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	update := Update{
-		LatestVersion: "0.1.2",
+		LatestVersion: "0.1.3",
 		DownloadUrl:   "https://passwall.io/download/passwall-macos/",
 		ProductUrl:    "https://signup.passwall.io",
 	}
