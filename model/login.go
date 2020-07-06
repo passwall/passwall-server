b356fe4050d9199ca89bd5456532156e2545ce2f
Recep Alaca <recepalacaa@gmail.com>
2020-07-06 09:01:18 +0300

    encryption and decryption for login model

--- model/login.go
+++ model/login.go
@@ -1,31 +1,75 @@
 package model
 
 import (
+	"encoding/base64"
+	"reflect"
 	"time"
+
+	"github.com/pass-wall/passwall-server/internal/app"
+	"github.com/spf13/viper"
 )
 
 // Login ...
 type Login struct {
-	ID        uint       `gorm:"primary_key" json:"id"`
-	CreatedAt time.Time  `json:"created_at"`
-	UpdatedAt time.Time  `json:"updated_at"`
-	DeletedAt *time.Time `json:"deleted_at"`
-	Title     string     `json:"title"`
-	URL       string     `json:"url"`
-	Username  string     `json:"username"`
-	Password  string     `json:"password"`
+	ID        uint       `gorm:"primary_key" json:"id" encrypt:"false"`
+	CreatedAt time.Time  `json:"created_at" encrypt:"true"`
+	UpdatedAt time.Time  `json:"updated_at" encrypt:"true"`
+	DeletedAt *time.Time `json:"deleted_at" encrypt:"true"`
+	Title     string     `json:"title" encrypt:"false"`
+	URL       string     `json:"url" encrypt:"true"`
+	Username  string     `json:"username" encrypt:"true"`
+	Password  string     `json:"password" encrypt:"true"`
 }
 
 type LoginDTO struct {
-	ID       uint   `json:"id"`
-	Title    string `json:"title"`
-	URL      string `json:"url"`
-	Username string `json:"username"`
-	Password string `json:"password"`
+	ID       uint   `json:"id" encrypt:"false"`
+	Title    string `json:"title" encrypt:"false"`
+	URL      string `json:"url" encrypt:"true"`
+	Username string `json:"username" encrypt:"true"`
+	Password string `json:"password" encrypt:"true"`
+}
+
+func encryptField(loginDTO LoginDTO) LoginDTO {
+	num := reflect.TypeOf(loginDTO).NumField()
+
+	var tagVal string
+
+	for i := 0; i < num; i++ {
+		tagVal = reflect.TypeOf(loginDTO).Field(i).Tag.Get("encrypt")
+		value := reflect.ValueOf(loginDTO).Field(i).String()
+
+		if tagVal == "true" {
+			value = base64.StdEncoding.EncodeToString(app.Encrypt(value, viper.GetString("server.passphrase")))
+			reflect.ValueOf(&loginDTO).Elem().Field(i).SetString(value)
+		}
+	}
+
+	return loginDTO
+}
+
+func decryptField(login Login) Login {
+	num := reflect.TypeOf(login).NumField()
+
+	var tagVal string
+
+	for i := 0; i < num; i++ {
+		tagVal = reflect.TypeOf(login).Field(i).Tag.Get("encrypt")
+		value := reflect.ValueOf(login).Field(i).String()
+
+		if tagVal == "true" {
+			valueByte, _ := base64.StdEncoding.DecodeString(value)
+			value = string(app.Decrypt(string(valueByte[:]), viper.GetString("server.passphrase")))
+		}
+	}
+
+	return login
 }
 
 // ToLogin ...
 func ToLogin(loginDTO *LoginDTO) *Login {
+
+	*loginDTO = encryptField(*loginDTO)
+
 	return &Login{
 		Title:    loginDTO.Title,
 		URL:      loginDTO.URL,
@@ -41,6 +85,8 @@ func ToLoginDTO(login *Login) *LoginDTO {
 	// for i := range trims {
 	// 	login.URL = strings.TrimPrefix(login.URL, trims[i])
 	// }
+
+	*login = decryptField(*login)
 
 	return &LoginDTO{
 		ID:       login.ID,
