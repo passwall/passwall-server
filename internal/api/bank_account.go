2694d5bbf4b0d286ac3b30ad97739588a09cc510
Recep Alaca <recepalacaa@gmail.com>
2021-01-05 12:56:57 +0300

    refactor bankaccount functions

--- internal/api/bank_account.go
+++ internal/api/bank_account.go
@@ -8,6 +8,7 @@ import (
 	"github.com/passwall/passwall-server/internal/app"
 	"github.com/passwall/passwall-server/internal/storage"
 	"github.com/passwall/passwall-server/model"
+	"github.com/spf13/viper"
 
 	"github.com/gorilla/mux"
 )
@@ -20,45 +21,43 @@ const (
 func FindAllBankAccounts(s storage.Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var err error
-		var bankAccounts []model.BankAccount
+		var bankAccountList []model.BankAccount
+
+		// Setup variables
+		transmissionKey := r.Context().Value("transmissionKey").(string)
 
 		fields := []string{"id", "created_at", "updated_at", "bank_name", "bank_code", "account_name", "account_number", "iban", "currency"}
 		argsStr, argsInt := SetArgs(r, fields)
 
+		// Get all bank accounts from db
 		schema := r.Context().Value("schema").(string)
-		bankAccounts, err = s.BankAccounts().FindAll(argsStr, argsInt, schema)
+		bankAccountList, err = s.BankAccounts().FindAll(argsStr, argsInt, schema)
 		if err != nil {
 			RespondWithError(w, http.StatusNotFound, err.Error())
 			return
 		}
 
 		// Decrypt server side encrypted fields
-		for i := range bankAccounts {
-			decBankAccount, err := app.DecryptModel(&bankAccounts[i])
+		for i := range bankAccountList {
+			uBankAccount, err := app.DecryptModel(&bankAccountList[i])
 			if err != nil {
 				RespondWithError(w, http.StatusInternalServerError, err.Error())
 				return
 			}
-			bankAccounts[i] = *decBankAccount.(*model.BankAccount)
+			bankAccountList[i] = *uBankAccount.(*model.BankAccount)
 		}
 
-		// Encrypt payload
-		var payload model.Payload
-		key := r.Context().Value("transmissionKey").(string)
-		encrypted, err := app.EncryptJSON(key, bankAccounts)
-		if err != nil {
-			RespondWithError(w, http.StatusInternalServerError, err.Error())
-			return
-		}
-		payload.Data = string(encrypted)
-
-		RespondWithJSON(w, http.StatusOK, payload)
+		RespondWithEncJSON(w, http.StatusOK, transmissionKey, bankAccountList)
 	}
 }
 
 // FindBankAccountByID finds a bank account by id
 func FindBankAccountByID(s storage.Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// Setup variables
+		transmissionKey := r.Context().Value("transmissionKey").(string)
+
+		// Check if id is integer
 		vars := mux.Vars(r)
 		id, err := strconv.Atoi(vars["id"])
 		if err != nil {
@@ -66,6 +65,7 @@ func FindBankAccountByID(s storage.Store) http.HandlerFunc {
 			return
 		}
 
+		// Find login by id from db
 		schema := r.Context().Value("schema").(string)
 		bankAccount, err := s.BankAccounts().FindByID(uint(id), schema)
 		if err != nil {
@@ -74,48 +74,45 @@ func FindBankAccountByID(s storage.Store) http.HandlerFunc {
 		}
 
 		// Decrypt server side encrypted fields
-		decBankAccount, err := app.DecryptModel(bankAccount)
+		uBankAccount, err := app.DecryptModel(bankAccount)
 		if err != nil {
 			RespondWithError(w, http.StatusInternalServerError, err.Error())
 			return
 		}
 
-		bankAccountDTO := model.ToBankAccountDTO(decBankAccount.(*model.BankAccount))
+		// Create DTO
+		bankAccountDTO := model.ToBankAccountDTO(uBankAccount.(*model.BankAccount))
 
-		// Encrypt payload
-		var payload model.Payload
-		key := r.Context().Value("transmissionKey").(string)
-		encrypted, err := app.EncryptJSON(key, bankAccountDTO)
-		if err != nil {
-			RespondWithError(w, http.StatusInternalServerError, err.Error())
-			return
-		}
-		payload.Data = string(encrypted)
-
-		RespondWithJSON(w, http.StatusOK, payload)
+		RespondWithEncJSON(w, http.StatusOK, transmissionKey, bankAccountDTO)
 	}
 }
 
 // CreateBankAccount creates a bank aaccount
 func CreateBankAccount(s storage.Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// Setup variables
+		env := viper.GetString("server.env")
+		transmissionKey := r.Context().Value("transmissionKey").(string)
 
-		payload, err := ToPayload(r)
-		if err != nil {
+		// Update request body according to env.
+		// If env is dev, then do nothing
+		// If env is prod, then decrypt payload with transmission key
+		if err := ToBody(r, env, transmissionKey); err != nil {
 			RespondWithError(w, http.StatusBadRequest, InvalidRequestPayload)
 			return
 		}
 		defer r.Body.Close()
 
-		// Decrypt payload
+		// Unmarshal request body to bankAccountDTO
 		var bankAccountDTO model.BankAccountDTO
-		key := r.Context().Value("transmissionKey").(string)
-		err = app.DecryptJSON(key, []byte(payload.Data), &bankAccountDTO)
-		if err != nil {
-			RespondWithError(w, http.StatusInternalServerError, err.Error())
+		decoder := json.NewDecoder(r.Body)
+		if err := decoder.Decode(&bankAccountDTO); err != nil {
+			RespondWithError(w, http.StatusBadRequest, "Invalid resquest payload")
 			return
 		}
+		defer r.Body.Close()
 
+		// Add new bankaccount to db
 		schema := r.Context().Value("schema").(string)
 		createdBankAccount, err := app.CreateBankAccount(s, &bankAccountDTO, schema)
 		if err != nil {
@@ -123,17 +120,17 @@ func CreateBankAccount(s storage.Store) http.HandlerFunc {
 			return
 		}
 
-		createdBankAccountDTO := model.ToBankAccountDTO(createdBankAccount)
-
-		// Encrypt payload
-		encrypted, err := app.EncryptJSON(key, createdBankAccountDTO)
+		// Decrypt server side encrypted fields
+		decBankAccount, err := app.DecryptModel(createdBankAccount)
 		if err != nil {
 			RespondWithError(w, http.StatusInternalServerError, err.Error())
 			return
 		}
-		payload.Data = string(encrypted)
 
-		RespondWithJSON(w, http.StatusOK, payload)
+		// Create DTO
+		createdBankAccountDTO := model.ToBankAccountDTO(decBankAccount.(*model.BankAccount))
+
+		RespondWithEncJSON(w, http.StatusOK, transmissionKey, createdBankAccountDTO)
 	}
 }
 
@@ -147,24 +144,26 @@ func UpdateBankAccount(s storage.Store) http.HandlerFunc {
 			return
 		}
 
-		// Unmarshal request body to payload
-		var payload model.Payload
-		decoder := json.NewDecoder(r.Body)
-		if err := decoder.Decode(&payload); err != nil {
+		// Setup variables
+		env := viper.GetString("server.env")
+		transmissionKey := r.Context().Value("transmissionKey").(string)
+
+		if err := ToBody(r, env, transmissionKey); err != nil {
 			RespondWithError(w, http.StatusBadRequest, InvalidRequestPayload)
 			return
 		}
 		defer r.Body.Close()
 
-		// Decrypt payload
+		// Unmarshal request body to loginDTO
 		var bankAccountDTO model.BankAccountDTO
-		key := r.Context().Value("transmissionKey").(string)
-		err = app.DecryptJSON(key, []byte(payload.Data), &bankAccountDTO)
-		if err != nil {
-			RespondWithError(w, http.StatusInternalServerError, err.Error())
+		decoder := json.NewDecoder(r.Body)
+		if err := decoder.Decode(&bankAccountDTO); err != nil {
+			RespondWithError(w, http.StatusBadRequest, "Invalid resquest payload")
 			return
 		}
+		defer r.Body.Close()
 
+		// Find login defined by id
 		schema := r.Context().Value("schema").(string)
 		bankAccount, err := s.BankAccounts().FindByID(uint(id), schema)
 		if err != nil {
@@ -172,23 +171,24 @@ func UpdateBankAccount(s storage.Store) http.HandlerFunc {
 			return
 		}
 
+		// Update login
 		updatedBankAccount, err := app.UpdateBankAccount(s, bankAccount, &bankAccountDTO, schema)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		updatedBankAccountDTO := model.ToBankAccountDTO(updatedBankAccount)
-
-		// Encrypt payload
-		encrypted, err := app.EncryptJSON(key, updatedBankAccountDTO)
 		if err != nil {
 			RespondWithError(w, http.StatusInternalServerError, err.Error())
 			return
 		}
-		payload.Data = string(encrypted)
 
-		RespondWithJSON(w, http.StatusOK, payload)
+		// Decrypt server side encrypted fields
+		decBankAccount, err := app.DecryptModel(updatedBankAccount)
+		if err != nil {
+			RespondWithError(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		// Create DTO
+		updatedBankAccountDTO := model.ToBankAccountDTO(decBankAccount.(*model.BankAccount))
+
+		RespondWithEncJSON(w, http.StatusOK, transmissionKey, updatedBankAccountDTO)
 	}
 }
 
